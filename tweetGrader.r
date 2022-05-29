1cd9cbfec386d0168c27787abad58fc50036b4d1
luketd <lukedamato99@gmail.com>
2022-05-29 15:12:58 -0400

    twitter grader 2.0
    Added Error handling when twitter accounts dont exist so it doesnt break the program
    Added general QOL so when I hand it to someone they can run it smoothly
    Made the write.csv function automatic to the date
    Hide the api keys

--- tweetGrader.r
+++ tweetGrader.r
@@ -1,24 +1,44 @@
-library (rsconnect)
-library(tidyverse)  
-library(rvest)  
-library(rtweet)
-library(dplyr) 
-library(purrr)
+{
+suppressMessages(suppressWarnings(require(rsconnect)))
+suppressMessages(suppressWarnings(require(tidyverse)))  
+suppressMessages(suppressWarnings(require(rvest)))
+suppressMessages(suppressWarnings(require(rtweet)))
+suppressMessages(suppressWarnings(require(dplyr)))
+suppressMessages(suppressWarnings(require(purrr)))
+suppressMessages(suppressWarnings(require(dotenv)))
+options(dplyr.summarise.inform = FALSE)
+  
+  
+#End date(Sunday date + 1 day)
+##################################################################
+#ENTER THE MONDAY DATE YOU WANT TO PULL IT FROM 
+#REMEMBER THAT ITS THE WEEK PRIOR MONDAY
+endDate <- as.POSIXct("2022-05-23  5:00:00")
 
 
+#enter the path that you want to copy the files to
+#when you copy the path from file explorer to R it will turn out as
+# C:\Users\Luke\Desktop\SHL\Twitter
+# change the \ to \\ and it will work
+filePath <- "C:\\Users\\Luke\\Desktop\\SHL\\Twitter\\"
+##################################################################
 
-## store api keys (these are fake example values; replace with your own keys)
-api_key <- "qiuhpw6xWBxLvhkbqlofyoN4m"
-api_secret_key <- "hOE5NGbaU3uSStw7o08eaPjeaR4iZ4MKzMLiLDrX2SjsswnosK"
-access_token <- "3270729511-UngtR9d7pPPNGWq6ZietCiZ1DminaRpxEsHuPIc"
-access_token_secret <- "qlUU8WbSswh0IrFYDhO888iJGZGmU291WH6USb5pifDAW"
+##----------------------------------------------------------------
+##          Getting the tokens and base variables set up         -
+##----------------------------------------------------------------
 
-# ## authenticate via web browser
-# token <- rtweet::create_token(
-#   app = "shlTweetGrader",
-#   consumer_key = api_key,
-#   consumer_secret = api_secret_key)
 
+load_dot_env()
+
+#api_key <- "qiuhpw6xWBxLvhkbqlofyoN4m"
+#api_secret_key <- "hOE5NGbaU3uSStw7o08eaPjeaR4iZ4MKzMLiLDrX2SjsswnosK"
+#access_token <- "3270729511-UngtR9d7pPPNGWq6ZietCiZ1DminaRpxEsHuPIc"
+#access_token_secret <- "qlUU8WbSswh0IrFYDhO888iJGZGmU291WH6USb5pifDAW"
+
+api_key <- Sys.getenv("api_key")
+api_secret_key <- Sys.getenv("api_secret_key")
+access_token <- Sys.getenv("access_token")
+access_token_secret <- Sys.getenv("access_token_secret")
 
 ## authenticate via web browser
 token <- create_token(
@@ -28,16 +48,9 @@ token <- create_token(
   access_token = access_token,
   access_secret = access_token_secret)
 
-#of pages(15 per page)
-
-#End date(Sunday date + 1 day)
-endDate <- as.POSIXct("2021-07-05  5:00:00")
-
 #Thread link
 threadLink <- "https://simulationhockey.com/showthread.php?tid=112765"
 threadLinkG <- paste(threadLink,"&page=%f", sep="")
-
-
 
 getLast <- threadLink %>%
    read_html() %>%
@@ -45,16 +58,18 @@ getLast <- threadLink %>%
    html_text()
  
 Pages <- as.integer(getLast)
-
-#endDate <- as.Date(endDate)
 pastDate <- endDate - 622800
 
 
+
+##----------------------------------------------------------------
+##        The function that runs the program for twitter         -
+##----------------------------------------------------------------
+
 getlist <- function(x) {
-  #x <- 2
   website <- sprintf(threadLinkG,x)
   url <- read_html(website)
-  #website
+  
   
   if(x==1){
     userName <- url %>%
@@ -71,7 +86,6 @@ getlist <- function(x) {
     html_nodes(".post_body > .mycode_url") %>%
     html_text()
   profile <- tolower(c(profile))
-  #print(profile)
   getProfile <- profile %>% as.data.frame() %>%
      separate(1, into = c("handle", "extra"), sep = "[?]")
   getProfile$handle <- gsub("https://www.twitter.com/", replacement = "", x = getProfile$handle)
@@ -82,91 +96,102 @@ getlist <- function(x) {
   getProfile$handle <- gsub("/with_replies", replacement = "", x = getProfile$handle)
   getProfile$handle <- gsub("/", replacement = "", x = getProfile$handle)
   
-  #print(getProfile)
   
   payment <- vector(mode ="integer", length=1)
   payInc <-1
+  payTime <- 0 
   for (value in getProfile$handle){
-    print(value)
+    #print(value)
     getuser <- rtweet::get_timeline(value, n = 150, check=FALSE, fast=TRUE)
     
-    filtered <- getuser %>% 
-      select(created_at, screen_name,hashtags, text ,replyToSN = reply_to_status_id) %>%
-      filter(created_at >= pastDate & created_at <= endDate & grepl("shlhockey", tolower(hashtags)) == TRUE)
-    
-      filtered$created_at <- filtered$created_at - 18000
-      twitter_name <- filtered$screen_name[1]
-      #print(twitter_name)
-    
-    
-    
-    if (dim(filtered)[1] == 0) {
-      payment[payInc] <- 0
-      payInc <- payInc +1
-    }else {
-      filtered$created_at <- as.Date(filtered$created_at, format="%y-%mm-%dd")
-      filtered["Payout"] <- 0
-      
-      
-      replyDate <- vector(mode = "character", length =1)
-      tweetDate <- vector(mode = "character", length =1)
-      reply <- 1
-      tweet <- 1
-      
-      
-      for (i in 1:length(filtered$created_at)){
-        #Check to see if it is a reply
-        if(is.na(filtered$replyToSN[i]) == TRUE) {
-          if (length(tweetDate) == 3) {
-            #checking if there is already 3 tweets claimed
-            filtered$Payout[i] <- 0
-          }else if (tweetDate[1]==""){
-            #gsub removes emoji characters
-            tweetDate[tweet] <- filtered$created_at[i]
-            tweet <- tweet +1
-            filtered$Payout[i] <- 150000
-          } else if (is.na(match(filtered$created_at[i], tweetDate)) == FALSE) {
-            #if date was already used
-            filtered$Payout[i] <- 0
-          } else {
-            tweetDate[tweet] <- filtered$created_at[i]
-            tweet <- tweet +1
-            filtered$Payout[i] <- 150000
-            
-            
-            
-          }
-          
-          #else, checks if it is a reply, if so enter the if statement
-        } else if (is.na(filtered$replyToSN[i]) == FALSE) { 
-          if (length(replyDate) == 3){
-            filtered$Payout[i] <- 0
-            #if there already are 3 replies that gets paid, then give that payout as 0
-          }else if (replyDate[1]==""){
-            #gsub removes emoji characters
-            replyDate[reply] <- filtered$created_at[i]
-            reply <- reply +1
-            filtered$Payout[i] <- 50000
-          } else if (is.na(match(filtered$created_at[i], replyDate)) == FALSE) {
-            #if date was already used
-            filtered$Payout[i] <- 0
-          } else {
-            replyDate[reply] <- filtered$created_at[i]
-            reply <- reply +1
-            filtered$Payout[i] <- 50000
-          }
-        } 
+    catchTwitter <- tryCatch(
+      {
+        filtered <- getuser %>% 
+          select(created_at, screen_name,hashtags, text ,replyToSN = reply_to_status_id) %>%
+          filter(created_at >= pastDate & created_at <= endDate & grepl("shlhockey", tolower(hashtags)) == TRUE)
         
+        filtered$created_at <- filtered$created_at - 18000
+        twitter_name <- filtered$screen_name[1]
+        if (dim(filtered)[1] == 0) {
+          payTime <- 0
+          #payment[payInc] <- 0
+          #payInc <- payInc +1
+          #print("0")
+        }else {
+          filtered$created_at <- as.Date(filtered$created_at, format="%y-%mm-%dd")
+          filtered["Payout"] <- 0
+          
+          
+          replyDate <- vector(mode = "character", length =1)
+          tweetDate <- vector(mode = "character", length =1)
+          reply <- 1
+          tweet <- 1
+          
+          
+          for (i in 1:length(filtered$created_at)){
+            #Check to see if it is a reply
+            if(is.na(filtered$replyToSN[i]) == TRUE) {
+              if (length(tweetDate) == 3) {
+                #checking if there is already 3 tweets claimed
+                filtered$Payout[i] <- 0
+              }else if (tweetDate[1]==""){
+                #gsub removes emoji characters
+                tweetDate[tweet] <- filtered$created_at[i]
+                tweet <- tweet +1
+                filtered$Payout[i] <- 150000
+              } else if (is.na(match(filtered$created_at[i], tweetDate)) == FALSE) {
+                #if date was already used
+                filtered$Payout[i] <- 0
+              } else {
+                tweetDate[tweet] <- filtered$created_at[i]
+                tweet <- tweet +1
+                filtered$Payout[i] <- 150000
+                
+                
+                
+              }
+              
+              #else, checks if it is a reply, if so enter the if statement
+            } else if (is.na(filtered$replyToSN[i]) == FALSE) { 
+              if (length(replyDate) == 3){
+                filtered$Payout[i] <- 0
+                #if there already are 3 replies that gets paid, then give that payout as 0
+              }else if (replyDate[1]==""){
+                #gsub removes emoji characters
+                replyDate[reply] <- filtered$created_at[i]
+                reply <- reply +1
+                filtered$Payout[i] <- 50000
+              } else if (is.na(match(filtered$created_at[i], replyDate)) == FALSE) {
+                #if date was already used
+                filtered$Payout[i] <- 0
+              } else {
+                replyDate[reply] <- filtered$created_at[i]
+                reply <- reply +1
+                filtered$Payout[i] <- 50000
+              }
+            } 
+          }
+          #gives the summation of the payout
+          temp <- filtered %>%
+            dplyr::group_by(screen_name) %>%
+            dplyr::summarise(Payout = as.double(format(sum(Payout), .groups = 'drop_last', scientific=F)))
+          
+          
+          payTime <- temp$Payout
+          #payment[payInc] <- temp$Payout
+          #payInc <- payInc +1
+          #print(temp$Payout)
+        }
+      },
+      error=function(cond){
+        print(paste0("Error getting user: ", value) )
+        payTime <- 0 
+      },
+      finally={
+        payment[payInc] <- payTime
+        payInc <- payInc + 1
       }
-      #gives the summation of the payout
-      temp <- filtered %>%
-        dplyr::group_by(screen_name) %>%
-        dplyr::summarize(Payout = format(sum(Payout), .groups = 'drop', scientific=F))
-    
-      
-      payment[payInc] <- temp$Payout
-      payInc <- payInc +1
-    }
+    )
   }
   data.frame(userName,getProfile$handle, payment)
   
@@ -175,104 +200,20 @@ getlist <- function(x) {
   
   
 }
-if (Pages == 1){
-  SHL <- purrr::map_df(1, getlist)
-} else {
-  SHL <- purrr::map_df(1:Pages, getlist)
-  duplicateOccur <- data.frame(table(SHL$getProfile.handle))
-  duplicateOccur[duplicateOccur$Freq > 1,]
-  UserOccur <- data.frame(table(SHL$userName))
-  UserOccur[UserOccur$Freq > 1,]
-  
+SHL <- purrr::map_df(1:Pages, getlist)
+
+
+write.csv(SHL, paste0(filePath, "Twitter", gsub(" 05:00:00", replacement = "", x = endDate),".csv" ), row.names = FALSE)
+
 }
+
+##---------------------------------------------------------------------------
+##        Checks for duplicates of twitter accounts and duplicate posts     -
+##---------------------------------------------------------------------------
+
+{
 duplicateOccur <- data.frame(table(SHL$getProfile.handle))
 duplicateOccur[duplicateOccur$Freq > 1,]
 UserOccur <- data.frame(table(SHL$userName))
 UserOccur[UserOccur$Freq > 1,]
-
-
-
-
-write.csv(SHL,"C:\\Users\\Luke\\Desktop\\SHL\\Twitter7-05csv", row.names = FALSE)
-
-
-
-
-
-getuser <- rtweet::get_timeline("benjanss1", n = 150, check=FALSE, fast=TRUE)
-
-filtered <- getuser %>% 
-  select(created_at, screen_name,hashtags, text ,replyToSN = reply_to_status_id) %>%
-  filter(created_at >= pastDate & created_at <= endDate & grepl("shlhockey", tolower(hashtags)) == TRUE)
-
-filtered$created_at <- filtered$created_at - 18000
-twitter_name <- filtered$screen_name[1]
-#print(twitter_name)
-
-
-
-if (dim(filtered)[1] == 0) {
-  payment[payInc] <- 0
-  payInc <- payInc +1
-}else {
-  filtered$created_at <- as.Date(filtered$created_at, format="%y-%mm-%dd")
-  filtered["Payout"] <- 0
-  
-  
-  replyDate <- vector(mode = "character", length =1)
-  tweetDate <- vector(mode = "character", length =1)
-  reply <- 1
-  tweet <- 1
-  
-  
-  for (i in 1:length(filtered$created_at)){
-    #Check to see if it is a reply
-    if(is.na(filtered$replyToSN[i]) == TRUE) {
-      if (length(tweetDate) == 3) {
-        #checking if there is already 3 tweets claimed
-        filtered$Payout[i] <- 0
-      }else if (tweetDate[1]==""){
-        #gsub removes emoji characters
-        tweetDate[tweet] <- filtered$created_at[i]
-        tweet <- tweet +1
-        filtered$Payout[i] <- 150000
-      } else if (is.na(match(filtered$created_at[i], tweetDate)) == FALSE) {
-        #if date was already used
-        filtered$Payout[i] <- 0
-      } else {
-        tweetDate[tweet] <- filtered$created_at[i]
-        tweet <- tweet +1
-        filtered$Payout[i] <- 150000
-        
-        
-        
-      }
-      
-      #else, checks if it is a reply, if so enter the if statement
-    } else if (is.na(filtered$replyToSN[i]) == FALSE) { 
-      if (length(replyDate) == 3){
-        filtered$Payout[i] <- 0
-        #if there already are 3 replies that gets paid, then give that payout as 0
-      }else if (replyDate[1]==""){
-        #gsub removes emoji characters
-        replyDate[reply] <- filtered$created_at[i]
-        reply <- reply +1
-        filtered$Payout[i] <- 50000
-      } else if (is.na(match(filtered$created_at[i], replyDate)) == FALSE) {
-        #if date was already used
-        filtered$Payout[i] <- 0
-      } else {
-        replyDate[reply] <- filtered$created_at[i]
-        reply <- reply +1
-        filtered$Payout[i] <- 50000
-      }
-    } 
-    
-  }
-  #gives the summation of the payout
-  temp <- filtered %>%
-    dplyr::group_by(screen_name) %>%
-    dplyr::summarize(Payout = format(sum(Payout), .groups = 'drop', scientific=F))
-  
 }
-
