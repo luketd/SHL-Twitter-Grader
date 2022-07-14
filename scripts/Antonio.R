06660d007af41c5a98ed606f9cd4e46e86c0184e
luketd <lukedamato99@gmail.com>
2022-07-13 21:06:14 -0400

    Update Antonio.R

--- scripts/Antonio.R
+++ scripts/Antonio.R
@@ -12,10 +12,10 @@ options(dplyr.summarise.inform = FALSE)
 conn_obj <- create_discord_connection(webhook = "https://discord.com/api/webhooks/989236566427832320/QeNAhpXULjACvgfeQzqFAhJA1rIg6oNFVVBq0BjTM5-lBBKVeC7Q-zq5B15kUNNaWwn4" , username = 'Is Antonio Funny', set_default = TRUE)
 
 
-api_key <- "qiuhpw6xWBxLvhkbqlofyoN4m"
-api_secret_key <- "hOE5NGbaU3uSStw7o08eaPjeaR4iZ4MKzMLiLDrX2SjsswnosK"
-access_token <- "3270729511-UngtR9d7pPPNGWq6ZietCiZ1DminaRpxEsHuPIc"
-access_token_secret <- "qlUU8WbSswh0IrFYDhO888iJGZGmU291WH6USb5pifDAW"
+api_key <- ${{ secrets.API_KEY }}
+api_secret_key <- ${{ secrets.API_SECRET_KEY }}
+access_token <-  ${{ secrets.ACCESS_TOKEN }}
+access_token_secret <- ${{ secrets.ACCESS_TOKEN_SECRET }}
 
 ## authenticate via web browser
 token <- create_token(
